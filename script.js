6770802a31fc3b1215918f3ecc08a1a3dd47aeea
Jtrinity87 <mercado.jacqueline.87@gmail.com>
2020-03-23 18:28:39 -0400

    pothole upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addNew(){
+    let name="name";
+    prompt ("Enter name of the Pothole");
+    return alert ("Thank you,we will add that to the list");
+    // result = window.prompt(name);
+}
